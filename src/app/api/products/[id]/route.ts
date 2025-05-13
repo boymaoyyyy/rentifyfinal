3d398eed12eaebb17e4482e31efca2b76fa7074b
boymaoyyyy <carpio.henrich123@gmail.com>
2025-05-13 14:40:25 +0800

    ashndfoasdh

--- src/app/api/products/[id]/route.ts
+++ src/app/api/products/[id]/route.ts
@@ -2,20 +2,19 @@ import { NextRequest, NextResponse } from 'next/server';
 import Product from '@/app/models/Product';
 import { connectToDB } from '@/app/lib/mongoose';
 
-// Delete a product by ID
+// DELETE a product by ID
 export async function DELETE(req: NextRequest, { params }: { params: { id: string } }) {
-  const { id } = params; // Extract the id from params
   await connectToDB();
 
   try {
-    const product = await Product.findByIdAndDelete(id);
+    const product = await Product.findByIdAndDelete(params.id); // Use params.id
 
     if (!product) {
       return NextResponse.json({ message: 'Product not found' }, { status: 404 });
     }
 
     return NextResponse.json({ message: 'Product deleted successfully' });
-  } catch (error: unknown) {
+  } catch (error) {
     return NextResponse.json({ message: 'Error deleting product', error }, { status: 500 });
   }
 }
